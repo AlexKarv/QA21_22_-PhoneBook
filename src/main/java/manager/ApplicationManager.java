5d9ebc73832e4409f2ea74becefd0687678dcb23
AlexKarv <alex.testengineer@gmail.com>
2023-12-10 20:56:06 +0200

    Push1

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ApplicationManager {
+    WebDriver driver;
+
+    public void init() {
+        driver = new ChromeDriver();
+        driver.navigate().to("https://telranedu.web.app/home");
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+}
